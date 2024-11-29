765929802d198b0db2c9a8429e9afeb6478a6383
mari <mariancaluag17@gmail.com>
2024-11-29 13:06:45 +0800

    update css

--- resources/views/layouts/client-layout.blade.php
+++ resources/views/layouts/client-layout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dentista Royale Dental Works @yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('/frameworks/bootstrap-5.3.3/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 
     <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}">
 
@@ -62,6 +63,8 @@
     <!-- sidebar -->
     <script src="{{ asset('assets/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
     <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 
