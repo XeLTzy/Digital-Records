c235fc28c5c023cae947a03a2a6f59435106c3b5
mari <mariancaluag17@gmail.com>
2024-11-29 13:20:07 +0800

    update

--- resources/views/layouts/client-layout.blade.php
+++ resources/views/layouts/client-layout.blade.php
@@ -9,10 +9,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
 
-    <!-- Responsive-->
-    <link rel="stylesheet" href=" {{ asset('/assets/css/responsive.css') }}">
     <!-- fevicon -->
     <link rel="icon" href="{{ asset('/assets/images/fevicon.png') }}" type="image/gif" />
     <!-- Scrollbar Custom CSS -->
@@ -21,7 +19,6 @@
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <!-- owl stylesheets -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="{{ asset('/assets/css/owl.theme.default.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
         media="screen">
 
