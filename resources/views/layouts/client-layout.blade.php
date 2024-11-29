a9e1ed0202eaed9cc6f6db1f1d951f98e614ac28
mari <mariancaluag17@gmail.com>
2024-11-29 12:56:34 +0800

    update css directory

--- resources/views/layouts/client-layout.blade.php
+++ resources/views/layouts/client-layout.blade.php
@@ -6,21 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dentista Royale Dental Works @yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('frameworks/bootstrap-5.3.3/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/frameworks/bootstrap-5.3.3/dist/css/bootstrap.min.css') }}">
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}">
 
     <!-- Responsive-->
-    <link rel="stylesheet" href=" {{ asset('assets/css/responsive.css') }}">
+    <link rel="stylesheet" href=" {{ asset('/assets/css/responsive.css') }}">
     <!-- fevicon -->
-    <link rel="icon" href="{{ asset('assets/images/fevicon.png') }}" type="image/gif" />
+    <link rel="icon" href="{{ asset('/assets/images/fevicon.png') }}" type="image/gif" />
     <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/jquery.mCustomScrollbar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/jquery.mCustomScrollbar.min.css') }}">
     <!-- Tweaks for older IEs-->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <!-- owl stylesheets -->
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/owl.theme.default.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
         media="screen">
 
